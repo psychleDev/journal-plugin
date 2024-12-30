05219102c5754e41e0ee8d1539c7c31570201559
psychleDev <psychledev@gmail.com>
2024-12-30 22:21:16 +0000

    Dec 30, 2024, 5:21 PM

--- includes/class-guided-journal.php
+++ includes/class-guided-journal.php
@@ -51,7 +51,7 @@ class GuidedJournal
             $roles = (array) $current_user->roles;
 
             // Only menoffire role can see Archive
-            if (!in_array('menoffire', $roles) && !current_user_can('administrator') && !in_array('subscriber', $roles)) {
+            if (!in_array('menoffire', $roles) && !current_user_can('administrator') && !current_user_can('subscriber')) {
                 wp_redirect(home_url('/'));
                 exit;
             }
