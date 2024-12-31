f5fe740609d32222dd672325d36d58dc2f2633d8
psychleDev <psychledev@gmail.com>
2024-12-31 18:00:17 +0000

    Dec 31, 2024, 1:00 PM

--- includes/class-guided-journal.php
+++ includes/class-guided-journal.php
@@ -52,15 +52,15 @@ class GuidedJournal
         });
 
         // Subscriber redirect to journal
-        // add_action('template_redirect', function () {
-        //     if (strpos($_SERVER['REQUEST_URI'], '/home/') !== false || is_front_page() || is_home()) {
-        //         $user = wp_get_current_user();
-        //         if (in_array('ignite30', (array) $user->roles)) {
-        //             wp_redirect(home_url('/grid'));
-        //             exit;
-        //         }
-        //     }
-        // });
+        add_action('template_redirect', function () {
+            if (strpos($_SERVER['REQUEST_URI'], '/home/') !== false || is_front_page() || is_home()) {
+                $user = wp_get_current_user();
+                if (in_array('ignite30', (array) $user->roles)) {
+                    wp_redirect(home_url('/grid'));
+                    exit;
+                }
+            }
+        });
     }
 
 
